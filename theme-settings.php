ff6e885a461fb9e209a1a07b49d7201dc517db3f
Brian Hay <brian@virtualrealms.com.au>
2013-12-27 16:45:14 +1000

    Added support for complex CSS selectors. Fixes #3

--- theme-settings.php
+++ theme-settings.php
@@ -22,23 +22,21 @@ function enso_light_form_system_theme_settings_alter(&$form, &$form_state)  {
     '#collapsible' => TRUE,
     '#collapsed' => FALSE
   );
-  $form['enso']['your_option'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Your Options'),
-    '#default_value' => theme_get_setting('your_option'),
-  );
   $form['enso']['css_selectors'] = array(
     '#type' => 'textfield',
-    '#title' => t('CSS Classes and IDs'),
+    '#size' => '120',
+    '#title' => t('CSS Selectors'),
     '#default_value' => theme_get_setting('css_selectors'),
-    '#description' => t('Enter a list of CSS classes and IDs, separated by commas <em>e.g. #header .footer etc</em>. For each class and ID specified here, further styling options will be displayed below after saving.'),
+    '#description' => t('Enter a list of CSS selectors, separated by commas <em>e.g. #header, .footer etc</em>. For each selector specified here, further styling options will be displayed below after saving.'),
   );
 
-  // Dynamically generate sets of form elements based on specified CSS classes and ids
-  $css_selectors = explode(',', preg_replace('/\s+/', '', theme_get_setting('css_selectors')));
+  // Dynamically generate sets of form elements based on user-defined CSS selectors
+  $css_selectors = explode(',', theme_get_setting('css_selectors'));
   foreach ($css_selectors as $css_selector) {
-    $css_selector_prefixes = array('.', '#');
-    $name = str_replace($css_selector_prefixes, '', $css_selector);
+    //$css_selector_prefixes = array('.', '#');
+    //$name = str_replace($css_selector_prefixes, '', $css_selector);
+    $css_selector = trim($css_selector);
+    $name = preg_replace('/[^a-z0-9]+/', '_', strtolower($css_selector));
     $form['enso'][$name] = enso_light_selector_form($name, $css_selector);
   }
 
@@ -51,10 +49,10 @@ function enso_light_settings_submit(&$form, &$form_state)  {
 
   // Save background images and generate CSS
   $css = '';
-  $css_selectors = explode(',', preg_replace('/\s+/', '', theme_get_setting('css_selectors')));
+  $css_selectors = explode(',', theme_get_setting('css_selectors'));
   foreach ($css_selectors as $css_selector) {
-    $css_selector_prefixes = array('.', '#');
-    $name = str_replace($css_selector_prefixes, '', $css_selector);
+    $css_selector = trim($css_selector);
+    $name = preg_replace('/[^a-z0-9]+/', '_', strtolower($css_selector));
     enso_light_selector_bg_save($form, $form_state, $name);
     $css .= enso_light_selector_generate_css($form_state, $name, $css_selector);
   }
